694477043f6b6b1b46f1bc627c0c1568a8ca435c
IlyaBayusov <bayusov96@mail.ru>
2025-05-07 19:08:00 +0300

    added pagination

--- src/components/AdminPage/pages/ProductsAdmin.tsx
+++ src/components/AdminPage/pages/ProductsAdmin.tsx
@@ -5,12 +5,6 @@ import { IProductInfo } from "@/interfaces";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { IoIosOptions } from "react-icons/io";
-import {
-  MdOutlineKeyboardArrowLeft,
-  MdOutlineKeyboardArrowRight,
-  MdOutlineKeyboardDoubleArrowLeft,
-  MdOutlineKeyboardDoubleArrowRight,
-} from "react-icons/md";
 
 export default function ProductsAdmin() {
   const [isActive, setIsActive] = useState(true);
@@ -39,24 +33,7 @@ export default function ProductsAdmin() {
 
   return (
     <div className="flex flex-col w-full px-3 bg-white">
-      <div className="mt-3 flex justify-center items-center gap-1">
-        <button className="px-2 py-1 border rounded-md">
-          <MdOutlineKeyboardDoubleArrowLeft className="h-5 w-5 p-px text-gray-400" />
-        </button>
-        <button className="px-2 py-1 border rounded-md">
-          <MdOutlineKeyboardArrowLeft className="h-5 w-5 p-px text-gray-400" />
-        </button>
-
-        <p className="text-black text-sm">1-10 из 24</p>
-
-        <button className="px-2 py-1 border rounded-md">
-          <MdOutlineKeyboardArrowRight className="h-5 w-5 p-px text-gray-400" />
-        </button>
-        <button className="px-2 py-1 border rounded-md">
-          <MdOutlineKeyboardDoubleArrowRight className="h-5 w-5 p-px text-gray-400" />
-        </button>
-      </div>
-
+      пагинация
       {products ? (
         <table className="text-black uppercase text-xs text-center -mx-3 mt-3">
           <thead>
